f8debbcf241cc0ca1bfc21221f298cf36211c621
Steve Gehrman <sgehrman@gmail.com>
2022-05-19 03:56:36 -0700

    fix

--- src/components/astra/astra-utils.tsx
+++ src/components/astra/astra-utils.tsx
@@ -1,5 +1,5 @@
 import { BaseToken, CardData } from '@infinityxyz/lib/types/core';
-import axios from 'axios';
+import axios, { AxiosRequestHeaders } from 'axios';
 import { DEFAULT_LIMIT, apiGet, getAuthHeaders, ApiResponse, LARGE_LIMIT, apiPost } from 'utils';
 import { Filter } from 'utils/context/FilterContext';
 import { RevealOrder, UpdateRankVisibility } from './be-types';
@@ -174,37 +174,12 @@ export const getReveals = async (user: string, startAfterTimestamp?: number): Pr
 
 export const httpGet = async (path: string, params: object): Promise<ApiResponse> => {
   try {
-    const userEndpointRegex = /\/(u|user)\//;
-    const publicUserEndpoint = /\/p\/u\//;
-    const requiresAuth = userEndpointRegex.test(path) && !publicUserEndpoint.test(path);
-
-    let authHeaders = {};
-    if (requiresAuth) {
-      authHeaders = await getAuthHeaders();
-    }
-
-    const response = await axios.get(path, { headers: authHeaders, params });
+    const headers = await authHeaders(path);
+    const response = await axios.get(path, { headers, params });
 
     return { status: response.status, error: '', result: response.data };
   } catch (error) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const err: any = error;
-    let status = 551;
-    let message = '';
-
-    if (err.response) {
-      message = err.response.data;
-      status = err.response.status;
-      console.log(err.response.headers);
-    } else if (err.request) {
-      console.log(err.request);
-      message = 'request error';
-    } else {
-      message = err.message;
-    }
-    console.log(err.config);
-
-    return { status: status, error: message };
+    return httpErrorResponse(error);
   }
 };
 
@@ -212,36 +187,49 @@ export const httpGet = async (path: string, params: object): Promise<ApiResponse
 
 export const httpPost = async (path: string, params: object): Promise<ApiResponse> => {
   try {
-    const userEndpointRegex = /\/(u|user)\//;
-    const publicUserEndpoint = /\/p\/u\//;
-    const requiresAuth = userEndpointRegex.test(path) && !publicUserEndpoint.test(path);
-
-    let authHeaders = {};
-    if (requiresAuth) {
-      authHeaders = await getAuthHeaders();
-    }
-
-    const response = await axios.post(path, { headers: authHeaders, params });
+    const headers = await authHeaders(path);
+    const response = await axios.post(path, params, { headers });
 
     return { status: response.status, error: '', result: response.data };
   } catch (error) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const err: any = error;
-    let status = 551;
-    let message = '';
-
-    if (err.response) {
-      message = err.response.data;
-      status = err.response.status;
-      console.log(err.response.headers);
-    } else if (err.request) {
-      console.log(err.request);
-      message = 'request error';
-    } else {
-      message = err.message;
-    }
-    console.log(err.config);
-
-    return { status: status, error: message };
+    return httpErrorResponse(error);
   }
+};
+
+// ===================================================================
+
+export const authHeaders = async (path: string): Promise<AxiosRequestHeaders> => {
+  const userEndpointRegex = /\/(u|user)\//;
+  const publicUserEndpoint = /\/p\/u\//;
+  const requiresAuth = userEndpointRegex.test(path) && !publicUserEndpoint.test(path);
+
+  let authHeaders = {};
+  if (requiresAuth) {
+    authHeaders = await getAuthHeaders();
+  }
+
+  return authHeaders;
+};
+
+// ===================================================================
+
+export const httpErrorResponse = (error: unknown) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const err: any = error;
+  let status = 551;
+  let message = '';
+
+  if (err.response) {
+    message = err.response.data;
+    status = err.response.status;
+    console.log(err.response.headers);
+  } else if (err.request) {
+    console.log(err.request);
+    message = 'request error';
+  } else {
+    message = err.message;
+  }
+  console.log(err.config);
+
+  return { status: status, error: message };
 };
