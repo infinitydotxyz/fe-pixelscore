e18cda0df2cf22445f7b4e217cdc6c28b08e4e35
Steve Gehrman <sgehrman@gmail.com>
2022-06-08 21:08:59 -0700

    tooltip

--- src/components/astra/collection-list-item.tsx
+++ src/components/astra/collection-list-item.tsx
@@ -3,6 +3,7 @@ import { BGImage } from 'components/common';
 import { CollectionInfo } from '../../utils/types/collection-types';
 import { twMerge } from 'tailwind-merge';
 import { inputBorderColor, selectionOutline } from 'utils/ui-constants';
+import { Toltip } from './toltip';
 
 interface Props2 {
   collection: CollectionInfo;
@@ -14,23 +15,28 @@ export const CollectionListItem = ({ collection, onClick, selected }: Props2) =>
   const avatarUrl = collection.bannerImage || collection.profileImage;
 
   return (
-    <div className="relative">
-      <div
-        className={twMerge(
-          'w-full cursor-pointer border bg-white rounded-lg overflow-clip h-24',
-          inputBorderColor,
-          selected ? selectionOutline : ''
-        )}
-        onClick={() => onClick(collection)}
-      >
-        <BGImage src={avatarUrl} />
+    // this wrapper div is needed, otherwise the tooltip gets top margin from the space-y value on the list
+    <div>
+      <Toltip className="z-50" content={collection.name} placement="right">
+        <div className="relative">
+          <div
+            className={twMerge(
+              'w-full cursor-pointer border bg-white rounded-lg overflow-clip h-24',
+              inputBorderColor,
+              selected ? selectionOutline : ''
+            )}
+            onClick={() => onClick(collection)}
+          >
+            <BGImage src={avatarUrl} />
 
-        <div className="absolute top-2 left-2 bottom-2 right-2 flex flex-col items-center justify-center">
-          <div className=" text-center tracking-tight leading-6 text-2xl font-bold text-white text-shadow">
-            {collection.name}
+            <div className="absolute top-2 left-2 bottom-2 right-2 flex flex-col items-center justify-center">
+              <div className=" text-center tracking-tight leading-6 text-2xl font-bold text-white text-shadow">
+                {collection.name}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </Toltip>
     </div>
   );
 };
