feffdf834655f8b4feb5faf382380c908043b92f
Steve Gehrman <sgehrman@gmail.com>
2022-06-19 01:28:08 -0700

    fix

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
     <div className={twMerge('flex px-8 py-2 items-center bg-slate-200 border-b shadow-md relative', inputBorderColor)}>
       <NextLink href="/" className="flex items-center">
         {/* <SVG.miniLogo className={largeIconButtonStyle} /> */}
-        <div className=" text-2xl font-bold">PixelScore</div>
+        <div className=" text-2xl font-bold">Pixelrank</div>
       </NextLink>
 
       <Spacer />
