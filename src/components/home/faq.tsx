3456f70ce01e87ec80ddeea521c38263d3338e4f
Steve Gehrman <sgehrman@gmail.com>
2022-06-19 23:54:20 -0700

    faq

--- src/components/home/faq.tsx
+++ src/components/home/faq.tsx
@@ -27,8 +27,8 @@ const faqs = [
 
 export const FAQ = () => {
   return (
-    <div className="flex dark:text-dark-body text-6xl">
-      <div className="  mr-14">
+    <div className="flex dark:text-dark-body text-6xl space-x-20">
+      <div className="  ">
         Frequently
         <br />
         Asked
@@ -36,33 +36,33 @@ export const FAQ = () => {
         <span className="text-blue-600">Questions</span>
       </div>
 
-      <div className="w-full       ">
-        <div className="     ">
-          <div className="  space-y-6 ">
-            {faqs.map((faq) => (
-              <Disclosure as="div" key={faq.question} className=" bg-gray-100 bg-opacity-10 p-10 rounded-3xl">
-                {({ open }) => (
-                  <>
-                    <dt className=" ">
-                      <Disclosure.Button className="text-left w-full flex justify-between items-start text-blue-600">
-                        <span className="font-medium text-3xl dark:text-dark-body">{faq.question}</span>
-                        <span className="ml-6 h-7 flex items-center">
-                          <ChevronDownIcon
-                            className={twMerge(open ? '-rotate-180' : 'rotate-0', 'h-10 w-10 transform ')}
-                            aria-hidden="true"
-                          />
-                        </span>
-                      </Disclosure.Button>
-                    </dt>
-                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                      <p className="text-base text-gray-500">{faq.answer}</p>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            ))}
-          </div>
-        </div>
+      <div className="w-full  space-y-6 ">
+        {faqs.map((faq) => (
+          <Disclosure
+            as="div"
+            key={faq.question}
+            className=" bg-gray-100 bg-opacity-10 p-10 rounded-3xl border-2 hover:border-blue-600 border-transparent hover:bg-blue-600 hover:bg-opacity-10  "
+          >
+            {({ open }) => (
+              <>
+                <dt className=" ">
+                  <Disclosure.Button className="text-left w-full flex justify-between items-start text-blue-600">
+                    <span className="font-medium text-3xl dark:text-dark-body">{faq.question}</span>
+                    <span className="ml-6 h-7 flex items-center">
+                      <ChevronDownIcon
+                        className={twMerge(open ? '-rotate-180' : 'rotate-0', 'h-10 w-10 transform ')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-6 pr-12">
+                  <p className="text-2xl dark:text-dark-body">{faq.answer}</p>
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        ))}
       </div>
     </div>
   );
