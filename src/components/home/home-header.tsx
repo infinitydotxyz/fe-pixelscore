905a33161f7ec4bb8c05dc3187c83f9c281a5db5
nneverlander <findadit@gmail.com>
2022-06-21 12:03:20 +0300

    mnor

--- src/components/home/home-header.tsx
+++ src/components/home/home-header.tsx
@@ -18,7 +18,7 @@ export const HomeHeader = () => {
         <div className="mt-28">
           <div className="block dark:text-dark-blue font-bold text-9xl">Pixelrank</div>
           <div className="mt-6 max-w-lg text-3xl dark:text-dark-body">
-            The world's only global NFT ranking. Pure Math. No bias.
+            The world's only global NFT ranking. Pure math. No bias.
           </div>
           <div className="mt-16">
             <div className="space-x-14 ">
