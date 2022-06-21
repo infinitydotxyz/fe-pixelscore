9d9959e4b5f1de23bdd30d659201840fff738e45
nneverlander <findadit@gmail.com>
2022-06-21 12:20:06 +0300

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        pixel: ['Pixel', 'sans-serif']
+      },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
