020de5aa569fd090b053d9ffad710868174bd796
nneverlander <findadit@gmail.com>
2022-07-21 20:15:11 +0200

    removes infinity logo

--- src/pages/password.tsx
+++ src/pages/password.tsx
@@ -1,4 +1,4 @@
-import { SVG, TextInputBox } from 'components/common';
+import { TextInputBox } from 'components/common';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSecurityContext } from 'utils/context/SecurityContext';
@@ -19,7 +19,6 @@ export const PasswordPage = () => {
     <div className="w-[100vw] h-[100vh] overflow-hidden grid place-content-center">
       <div className="flex flex-col gap-2 items-center">
         <div className="drop-shadow-2xl bg-white rounded-3xl flex flex-col items-center mx-0 my-4 p-8">
-          <SVG.miniLogo className="h-16 w-16" />
           <div className="text-center mt-2 mb-6 text-xl font-bold">Password</div>
           <TextInputBox
             label="Password"
